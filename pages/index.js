233f38fb064996d5c404b1cdd1a086c77a099c0e
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-03 22:02:27 +0700

    added font and style

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <div className="bg-gradient-to-b from-gray-600 to-gray-900 min-h-screen">
+            <nav>
+                <div className="container mx-auto">
+                    <div className="flex">
+                        <div className="w-2/12">
+                            Bastian
+                        </div>
+                        <div className="w-6/12">
+                            Nav
+                        </div>
+                        <div className="w-4/12">
+
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
